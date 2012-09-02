424d82436bca9431c3d2465db1bc924274cf04b8
Zhenwei Liu <cngdflzw@gmail.com>
2012-09-03 03:41:14 +0800

    new file:   backup/bills.sql

---
+++ backup/bills.sql
@@ -0,0 +1,47 @@
+-- MySQL dump 10.13  Distrib 5.5.24, for Win64 (x86)
+--
+-- Host: localhost    Database: bills
+-- ------------------------------------------------------
+-- Server version	5.5.24
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bill`
+--
+
+DROP TABLE IF EXISTS `bill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bill` (
+  `billId` int(11) NOT NULL AUTO_INCREMENT,
+  `payer` enum('刘振威','叶扬帆') NOT NULL,
+  `totalPrice` float NOT NULL COMMENT '金额',
+  `date` date NOT NULL COMMENT '账单日期',
+  `owner` enum('公共','刘振威','叶扬帆') NOT NULL COMMENT '物品所有者',
+  `name` varchar(50) NOT NULL COMMENT '购买物品名称',
+  `amount` float NOT NULL COMMENT '物品数量',
+  `price` float NOT NULL COMMENT '物品单价',
+  PRIMARY KEY (`billId`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='账单表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-09-03  3:40:11
